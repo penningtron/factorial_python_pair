854b7312c63aef739eb70934307dcc50755e7481
nathan pennington <nathantp@gmail.com>
2024-02-01 16:04:06 -0600

    pasted code

--- factorial.py
+++ factorial.py
@@ -1,3 +1,13 @@
 def factorial(num):
+	  # if num is 0 or 1 ... return 1
+    fac = 1
+
+
+    for i in range(num, 0, -1):
+        fac *= i
+        # print(i)
+        # print(f"the factorial of {num} is {fac}")
+        # print(fac * i)
+    return fac
 	# your code here
-	pass
+print(factorial(4))	
